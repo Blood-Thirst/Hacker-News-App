ed15a80f1868ee12277ec376026c37b73d1a2928
Ayush Dewangan <ayushdewangan12@gmail.com>
2023-01-19 16:47:20 +0530

    http -> https

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
-export const News = (query,tags,time,searchByDate) => {
-  
-  const currentTime = new Date().getTime()/1000;
-  const difference_time = (time==0)?0:(currentTime-time);
-  console.log(searchByDate)
-  const sortingType = (searchByDate)?"search_by_date":"search";
-  const url ="http://hn.algolia.com/api/v1/"+sortingType+ "?query="+query +"&tags=" +tags+"&numericFilters=created_at_i>"+difference_time;
+export const News = (query, tags, time, searchByDate) => {
+  const currentTime = new Date().getTime() / 1000;
+  const difference_time = time == 0 ? 0 : currentTime - time;
+  console.log(searchByDate);
+  const sortingType = searchByDate ? "search_by_date" : "search";
+  const url =
+    "https://hn.algolia.com/api/v1/" +
+    sortingType +
+    "?query=" +
+    query +
+    "&tags=" +
+    tags +
+    "&numericFilters=created_at_i>" +
+    difference_time;
   console.log(url);
   return url;
-;
 };
