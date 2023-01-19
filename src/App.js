e31a051e38a3402efee014f8cb791b842eb9d295
Ayush Dewangan <ayushdewangan12@gmail.com>
2023-01-19 16:59:34 +0530

    add meta tag

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Filter from "./components/Filter";
 function App() {
   return (
     <div className="App" style={{ marginLeft: "0%" }}>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      ></meta>
       <Header />
       <Filter />
       <NewsTable />
